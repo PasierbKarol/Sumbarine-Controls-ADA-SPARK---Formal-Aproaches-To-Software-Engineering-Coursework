df26cee56e3ca2fb031927bdc28dc4b5fba8fa6d
pasierbkarol <pasierbkarol@gmail.com>
2019-04-05 19:08:27 +0100

    Airlock and door related functions are completed and prooved

--- src/submarine_controls.adb
+++ src/submarine_controls.adb
@@ -1,41 +1,44 @@
 package body Submarine_Controls with SPARK_Mode is
 
   
-   procedure closeInnerAirlock is      
-   begin
+   procedure closeInnerAirlock is begin
       if outerAirlockState = True then        
          innerAirlockState := True;      
       end if;      
    end closeInnerAirlock;
    
-   procedure openInnerAirlock is      
-   begin
-      if outerAirlockState = True then
+   procedure openInnerAirlock is begin
+      if outerAirlockState = True and airlockDoorsLocked = False then
          innerAirlockState := False;     
       end if;      
    end openInnerAirlock;
    
-   procedure closeOuterAirlock is      
-   begin
-      outerAirlockState := True;    
+   procedure closeOuterAirlock is begin
+      if innerAirlockState = True then
+         outerAirlockState := True;    
+      end if;      
    end closeOuterAirlock;
    
-   procedure openOuterAirlock is      
-   begin
-      if innerAirlockState = True then
+   procedure openOuterAirlock is begin
+      if innerAirlockState = True and airlockDoorsLocked = False then
          outerAirlockState := False;     
       end if;      
    end openOuterAirlock;
    
+   procedure lockAirlockDoors is begin
+      if innerAirlockState = True and outerAirlockState = True then
+         airlockDoorsLocked := True;
+      end if;      
+   end lockAirlockDoors;
+     
    
    
    
    
    
-   
-   function canOperateSubmarine (innerAirlockState : AirlockState; 
-                                 outerAirlockState : AirlockState) 
-                                 return AirlockState is
+   function canOperateSubmarine (innerAirlockState : Boolean; 
+                                 outerAirlockState : Boolean) 
+                                 return Boolean is
    begin
       if innerAirlockState = True and outerAirlockState = True then      
         return True;
--- src/submarine_controls.ads
+++ src/submarine_controls.ads
@@ -1,35 +1,52 @@
 package Submarine_Controls with SPARK_Mode is
 
-   type AirlockState is new Boolean;
-   innerAirlockState : AirlockState; --True closed, False open
-   outerAirlockState : AirlockState; --True closed, False open
+   innerAirlockState : Boolean; --True closed, False open
+   outerAirlockState : Boolean; --True closed, False open
+   airlockDoorsLocked : Boolean; --True no doors can be opened
    
    --at least one airlock door closed at all times
+   --procedures below ensure that at least one airlock door is closed at all times
    procedure closeInnerAirlock  with
+     Global => (In_Out => innerAirlockState, Input => outerAirlockState),
      Pre => outerAirlockState = True,
      Post => innerAirlockState = True and then outerAirlockState = True;
    
    procedure openInnerAirlock  with
-     Pre => outerAirlockState = True and then innerAirlockState = True,
+     Global => (In_Out => innerAirlockState, 
+                Input => (outerAirlockState, airlockDoorsLocked)),
+     Pre => airlockDoorsLocked = False and then 
+            outerAirlockState = True and then innerAirlockState = True,
      Post => innerAirlockState = False and then outerAirlockState = True;
    
    procedure closeOuterAirlock  with
+     Global => (In_Out => outerAirlockState, Input => innerAirlockState),
      Pre => innerAirlockState = True,
      Post => outerAirlockState = True and then innerAirlockState = True;
 
    procedure openOuterAirlock  with
-     Pre => outerAirlockState = True and then innerAirlockState = True,
+     Global => (In_Out => outerAirlockState, 
+                Input => (innerAirlockState, airlockDoorsLocked)),
+     Pre => airlockDoorsLocked = False and then 
+            outerAirlockState = True and then innerAirlockState = True,
      Post => innerAirlockState = True and then outerAirlockState = False;
    
+   procedure lockAirlockDoors with
+     Global => (Input => (innerAirlockState, outerAirlockState), 
+                In_Out => airlockDoorsLocked),
+     Pre => innerAirlockState = True and then outerAirlockState = True,
+     Post => innerAirlockState = True and then outerAirlockState = True 
+     and then airlockDoorsLocked = True;
    
    
    
    
    
    
-   function canOperateSubmarine (innerAirlockState : in AirlockState; 
-                                 outerAirlockState : in AirlockState) 
-                                 return AirlockState with
+   
+   
+   function canOperateSubmarine (innerAirlockState : in Boolean; 
+                                 outerAirlockState : in Boolean) 
+                                 return Boolean with
      Pre => innerAirlockState = True and outerAirlockState = True,
      Post => canOperateSubmarine'Result = True or canOperateSubmarine'Result = False;
    
