7751b18835047a0eb20a5ea9590c9c0a771d9276
pasierbkarol <pasierbkarol@gmail.com>
2019-04-07 10:20:53 +0100

    Reorganized code, temporarily commented out problematic spots, added description comments

--- src/submarine_controls.adb
+++ src/submarine_controls.adb
@@ -1,7 +1,9 @@
      with Ada.Text_IO; use Ada.Text_IO;
 package body Submarine_Controls with SPARK_Mode is
 
-  
+   --Airlock procedures ===============================================================
+   --==== Description ===--
+   --following procedures ensure that at least one airlock door must be closed at all times
    procedure closeInnerAirlock is begin
       if outerAirlockState = True then        
          innerAirlockState := True;      
@@ -26,47 +28,63 @@ package body Submarine_Controls with SPARK_Mode is
       end if;      
    end openOuterAirlock;
    
+   --==== Description ===--
+   --this procedure ensures that airlock can be locked only when both airlock doors are closed
+   --every sea operation is checked against this status
    procedure lockAirlockDoors is begin
       if innerAirlockState = True and outerAirlockState = True then
          airlockDoorsLocked := True;
       end if;      
    end lockAirlockDoors;
-     
+    
+   --==== Description ===--
+   --each operation on the sea decreases the oxygen
    procedure decreaseOxygen is begin
       --if oxygen is running low the warning is shown
 --        if oxygenLevel <= 500 then
 --           displayOxygenWarning;
 --        end if;
       
-      if oxygenLevel >= 100 and airlockDoorsLocked = True and submarineSubmerged = True then
+      if oxygenLevel >= 100 and airlockDoorsLocked = True then
          oxygenLevel := oxygenLevel - 100;             
       end if;      
    end decreaseOxygen;
    
-   procedure checkOxygenAndReactorStatus is begin
-      --if decreasing oxygen lead to the point when it's 0 submarine has to resurface
-      --therefore depth goes back to initial level
-      if oxygenLevel = 0 and currentDepth <= maximumDepth and currentDepth >= 0 then
+   --==== Description ===--
+   --if decreasing oxygen lead to the point when it's 0 submarine has to resurface
+   --therefore depth goes back to initial level
+   procedure checkOxygenStatus is begin      
+      --if oxygenLevel = 0 and currentDepth <= maximumDepth and currentDepth >= 0 then
+      if oxygenLevel = 0 then
          --if oxygen runs out submarine has to resurface
             currentDepth := 0; --submarine resurfaced
             oxygenLevel := maximumOxygen; --oxygen is refilled
          end if;   
-   end checkOxygenAndReactorStatus;
+   end checkOxygenStatus;
       
 --     procedure displayOxygenWarning is begin
 --                 Put_Line("WARNING! Oxygen level is low!");
 --     end displayOxygenWarning;
    
-   procedure diveDeeper is begin
-      --increasing depth, 0 is on surface
-      if currentDepth <= maximumDepth - 100 and oxygenLevel >= 100 and airlockDoorsLocked = True and submarineSubmerged = True then
+--     procedure diveDeeper is begin
+--        increaseDepth;
+--        checkOxygenAndReactorStatus;
+--     end diveDeeper;
+   
+   --Depth procedures ========================================================
+   --==== Description ===--
+   --increasing depth by 100 point at each operation, 0 is on surface, 5000 is maximum
+   --altering depth consumes oxygen
+   --if submarine reaches maximum depth it cannot go lower
+   --therefore current depth must be always at least 100 point less than the maximum
+   procedure increaseDepth is begin
+      
+      if currentDepth <= maximumDepth - 100 and oxygenLevel >= 100 and airlockDoorsLocked = True then
          currentDepth := currentDepth + 100;
-         decreaseOxygen; --calling decrease oxygen
+         --decreaseOxygen; --calling decrease oxygen
       end if;
-      --checking oxygen and reactor status after each operation
-      checkOxygenAndReactorStatus;
-        
-   end diveDeeper;
+      --checking oxygen and reactor status after each operation       
+   end increaseDepth;
    
    
 
